4ce7f3d420907fa26b8dabd52fc84c8ad129d86b
iamlongphan <themugglecow@gmail.com>
2024-01-09 05:51:08 -0800

    Fixed some long-standing bugs regarding deletion and changing and general clean-up

---
+++ src/main/java/application/AccountController.java
@@ -0,0 +1,297 @@
+package application;
+
+import edu.sjsu.yazdankhah.crypto.util.PassUtil;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class AccountController {
+
+    @FXML
+    Button confirmPasswordB;
+    @FXML
+    Button confirmSecurityB;
+    @FXML
+    Button cancelB;
+    @FXML
+    Button deleteAccountB;
+    @FXML
+    TextField oldpass;
+    @FXML
+    TextField newpass;
+    @FXML
+    TextField cityname;
+    @FXML
+    Label errorMessageLabel;
+
+    @FXML
+    Label cityErrorMessageLabel;
+
+    String currentQans;
+
+
+
+    /**
+     * Closes the account settings page.
+     */
+    @FXML
+    public void cancelButton() {
+        try {
+            Parent root = FXMLLoader.load(Main.class.getResource("courseViewer.fxml"));
+            Stage stage = (Stage) this.cancelB.getScene().getWindow();
+            stage.setTitle("Course Viewer");
+            stage.setScene(new Scene(root, 1024, 768));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException var5) {
+            var5.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Validates whether an account exists
+     *
+     * @param oldpassw The old password
+     * @return Returns true if the password matches the one in the User database.
+     * @throws FileNotFoundException file not found
+     */
+    public boolean validatespass(String oldpassw) throws FileNotFoundException {
+        File database = new File("users.TXT");
+        File readUsername = new File("currentUser.txt");
+        Scanner readDatabase = new Scanner(database);
+        Scanner readUser = new Scanner(readUsername);
+        String currentUserName = readUser.nextLine();
+        while (readDatabase.hasNextLine()) {
+            String currentLine = readDatabase.nextLine();
+            String[] details = currentLine.split(",");
+            String usernameOfAccount = details[0];
+            String realOldPass = details[1];
+            PassUtil passUtil = new PassUtil();
+            realOldPass = passUtil.decrypt(realOldPass);
+            if (currentUserName.contains(usernameOfAccount)) {
+                if (realOldPass.equals(oldpassw)) {
+                    currentQans = details[2];
+                    readDatabase.close();
+                    return true;
+                }
+            }
+
+        }
+        readDatabase.close();
+        readUser.close();
+        return false;
+
+    }
+
+    /**
+     * Is the main function for the change of the password.  Changes the password in the file.
+     *
+     * @throws IOException In case we can't read
+     */
+
+    public void changePass() throws IOException {
+        //read from file the new pass
+        String oldPassword = oldpass.getText();
+        String newPassword = newpass.getText();
+
+        File usersFile = new File("users.TXT");
+
+
+        File currentUserFile = new File("currentUser.TXT");
+        Scanner currentUserReader = new Scanner(currentUserFile);
+        String usernameOfCurrent = currentUserReader.nextLine();
+        currentUserReader.close();
+
+        if (validatespass(oldPassword)) {
+            removeLineFromFile(usernameOfCurrent);
+
+            BufferedWriter passwordRewriter = new BufferedWriter(new FileWriter(usersFile, true));
+            Scanner passwordReader = new Scanner(usersFile);
+            PassUtil passUtil = new PassUtil();
+            newPassword = passUtil.encrypt(newPassword);
+            passwordRewriter.write(usernameOfCurrent + ",");
+            passwordRewriter.write(newPassword + ",");
+            passwordRewriter.write(currentQans + ",");
+            passwordRewriter.write("\n");
+
+            passwordRewriter.close();
+            passwordReader.close();
+
+
+            System.out.println("Change password");
+        } else {
+            errorMessageLabel.setText("Invalid Please try again!");
+            System.out.println("Password is not Correct");
+        }
+
+
+    }
+
+    /**
+     * Is the main function for the button that changes the security answer for the account.
+     *
+     * @throws IOException In case we cant read
+     */
+    public void changeSecurityAnswer() throws IOException {
+        File currentUserFile = new File("currentUser.TXT");
+        File usersFile = new File("users.TXT");
+
+        Scanner currentUserReader = new Scanner(currentUserFile);
+        String usernameOfCurrent = currentUserReader.nextLine();
+        currentUserReader.close();
+
+        String passwordTosave = "";
+        Scanner savePasswordHere = new Scanner(usersFile);
+        while (savePasswordHere.hasNextLine()) {
+            String[] details = savePasswordHere.nextLine().split(",");
+            if (details[0].equals(usernameOfCurrent)) {
+                passwordTosave = details[1];
+                System.out.println(passwordTosave);
+            }
+        }
+        savePasswordHere.close();
+        removeLineFromFile(usernameOfCurrent);
+
+        String newAnswer = cityname.getText();
+        if (cityname.getText().isEmpty()) {
+            cityErrorMessageLabel.setText("Invalid Please Fill City Name");
+        } else {
+            BufferedWriter qAnsRewriter = new BufferedWriter(new FileWriter(usersFile, true));
+            qAnsRewriter.write(usernameOfCurrent + ",");
+            qAnsRewriter.write(passwordTosave + ",");
+            qAnsRewriter.write(newAnswer + ",");
+            qAnsRewriter.write("\n");
+            qAnsRewriter.close();
+
+        }
+
+
+    }
+
+    /**
+     * Removes information saved to the database about an account. Removes any line from the flat file database.
+     *
+     * @param courseToRemove input a String name of the course we wish to remove
+     */
+    public void removeLineFromFile(String courseToRemove) {
+
+        try {
+
+            File userReader = new File("currentUser.txt");
+            Scanner readUser = new Scanner(userReader);
+
+            readUser.close();
+
+
+            File inFile = new File("users.txt");
+            BufferedReader br = new BufferedReader(new FileReader(inFile));
+
+            if (!inFile.isFile()) {
+                System.out.println("Parameter is not an existing file");
+                return;
+            }
+
+            //Construct the new file that will later be renamed to the original filename.
+            File tempFile = new File(inFile.getAbsolutePath() + ".tmp");
+
+            //BufferedReader br = new BufferedReader(new FileReader());
+            PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+
+            String line;
+            //Read from the original file and write to the new
+            //unless content matches data to be removed.
+            while ((line = br.readLine()) != null) {
+
+                if (!line.trim().contains(courseToRemove)) {
+
+                    pw.println(line);
+                    pw.flush();
+                }
+
+
+            }
+            pw.close();
+            br.close();
+
+            //Delete the original file
+            inFile.delete();
+            //Rename the new file to the original file
+            tempFile.renameTo(inFile);
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    /**
+     * The function for the deletion of an account. Removes all existence of an account.
+     *
+     * @throws FileNotFoundException in case file no found
+     */
+    public void deleteAccount() throws IOException {
+
+        File current = new File("currentUser.txt");
+        Scanner readUser = new Scanner(current);
+        String currentUserLogged = readUser.next();
+        File checkIfFileExists = new File(currentUserLogged + ".txt");
+        File deleteDirectory2 = new File("userData/" + currentUserLogged);
+
+        if (deleteDirectory2.exists()) {
+            deleteDirectory(deleteDirectory2);
+            deleteDirectory2.delete();
+        }
+        if (checkIfFileExists.exists()) {
+            checkIfFileExists.delete();
+        }
+        removeLineFromFile(currentUserLogged);
+
+        Parent root;
+        try {
+            File file1 = new File("currentUser.txt");
+
+            BufferedWriter buff1 = new BufferedWriter(new FileWriter(file1));
+            buff1.write("");
+            buff1.close();
+            root = FXMLLoader.load(Main.class.getResource("login.fxml"));
+            Stage stage = (Stage) deleteAccountB.getScene().getWindow();
+            stage.setTitle("Indexify");
+            stage.setScene(new Scene(root, 530, 400));
+            stage.setResizable(false);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Is a function created in order to delete the files out of the userData directory in the case that the account is deleted.
+     * @param file Delete file with said file name
+     */
+    public static void deleteDirectory(File file) {
+
+        File[] list = file.listFiles();
+        if (list != null) {
+            for (File temp : list) {
+                System.out.println("Visit " + temp);
+                temp.delete();
+                deleteDirectory(temp);
+            }
+        }
+
+
+
+    }
+}
---
+++ src/main/java/application/IndexCardController.java
@@ -0,0 +1,442 @@
+package application;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class IndexCardController implements Initializable {
+    @FXML
+    Rectangle card;
+
+    @FXML
+    ImageView editButton;
+
+    @FXML
+    CheckBox checkBox;
+
+    @FXML
+    Button createCard;
+
+    @FXML
+    Button nextButton;
+
+    @FXML
+    Button previousButton;
+
+    @FXML
+    Label text;
+
+    @FXML
+    Group scene;
+
+    @FXML
+    TextField counterBox;
+
+    @FXML
+    Button reviewLearnedB;
+
+    Set set;
+
+    String currentUser;
+    String currentCourse;
+    Boolean currentCheck;
+
+
+    /**
+     * Main function for the edit button in the IndexCardViewer FXML file.  Allows the card to edit itself.
+     * @param e Event
+     */
+    @FXML
+    public void editButtonClicked(Event e) {
+
+        Stage stage = new Stage();
+        stage.setTitle("Edit Card");
+
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 400, 300);
+
+        Label termLabel = new Label("Term: ");
+        TextField termField = new TextField(set.getCard().getFront());
+
+        Label definitionLabel = new Label("Definition: ");
+        TextField definitionField = new TextField(set.getCard().getBack());
+
+        Button submit = new Button("Submit");
+        submit.setStyle("-fx-text-fill: white; -fx-background-color: #a6051a");
+
+        submit.setOnAction((actionEvent) -> {
+            if(!(termField.getText().isEmpty() || definitionField.getText().isEmpty())){
+                //boolean selected = !checkBox.isSelected();
+                removeLineFromFile(set.getCard().getFront());
+                set.updateCard(termField.getText(), definitionField.getText(), checkBox.isSelected());
+
+                File currentCourseFile = new File("userData/" + currentUser + "/" + currentUser+currentCourse+".txt");
+                BufferedWriter currentCourseWriter;
+                try {
+                    currentCourseWriter = new BufferedWriter(new FileWriter(currentCourseFile,true));
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                currentCheck = checkBox.isSelected();
+                try {
+                    currentCourseWriter.write(termField.getText()+","+definitionField.getText()+","+currentCheck.toString()+",");
+                    currentCourseWriter.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                stage.close();
+            }
+        });
+
+        HBox termBox = new HBox();
+        termLabel.setPadding(new Insets(5, 20, 0, 0));
+        termBox.getChildren().addAll(termLabel, termField);
+        termBox.setPadding(new Insets(20, 0, 20, 30));
+
+        HBox defBox = new HBox();
+        definitionLabel.setPadding(new Insets(5, 20, 0, 0));
+        defBox.getChildren().addAll(definitionLabel, definitionField);
+        defBox.setPadding(new Insets(20, 0, 20, 30));
+
+        HBox submitBox = new HBox();
+        submitBox.getChildren().addAll(submit);
+        submitBox.setPadding(new Insets(20, 0, 20, 30));
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(termBox, defBox, submitBox);
+
+        root.getChildren().addAll(vbox);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Method for the creation of a card.  Will open another stage in order to create a new card.
+     * @param e Event
+     */
+    @FXML
+    public void createButtonClicked(Event e){
+
+        Stage stage = new Stage();
+        stage.setTitle("Create Card");
+
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 400, 300);
+
+        Label termLabel = new Label("Term: ");
+        TextField termField = new TextField();
+
+        Label definitionLabel = new Label("Definition: ");
+        TextField definitionField = new TextField();
+
+        Button submit = new Button("Create Card");
+        submit.setStyle("-fx-text-fill: white; -fx-background-color: #a6051a");
+
+        submit.setOnAction((actionEvent) -> {
+            if(!(termField.getText().isEmpty() || definitionField.getText().isEmpty())){
+                set.addCard(termField.getText(), definitionField.getText(), false);
+                File courseRead = new File("userData/" + currentUser + "/" + currentUser+currentCourse+".txt");
+                try {
+                    BufferedWriter courseFileWriter = new BufferedWriter(new FileWriter(courseRead,true));
+                    courseFileWriter.write(termField.getText()+","+definitionField.getText()+","+"false,");
+                    courseFileWriter.write("\n");
+                    courseFileWriter.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                stage.close();
+            }
+        });
+
+        HBox termBox = new HBox();
+        termLabel.setPadding(new Insets(5, 20, 0, 0));
+        termBox.getChildren().addAll(termLabel, termField);
+        termBox.setPadding(new Insets(20, 0, 20, 30));
+
+        HBox defBox = new HBox();
+        definitionLabel.setPadding(new Insets(5, 20, 0, 0));
+        defBox.getChildren().addAll(definitionLabel, definitionField);
+        defBox.setPadding(new Insets(20, 0, 20, 30));
+
+        HBox submitBox = new HBox();
+        submitBox.getChildren().addAll(submit);
+        submitBox.setPadding(new Insets(20, 0, 20, 30));
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(termBox, defBox, submitBox);
+
+        root.getChildren().addAll(vbox);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Delete function for the delete button of the index cards.
+     * @param e Event
+     */
+    @FXML
+    public void deleteButtonClicked(Event e) {
+        String nameOfCard = set.getCard().getFront();
+        removeLineFromFile(nameOfCard);
+        set.deleteCard(set.getCard());
+    }
+
+    @FXML
+    public void flip(){
+        set.flipCurrentCard();
+    }
+
+    /**
+     * Is the main function for the checkboxes on each of the index cards, returns whether or not the index cards are checked as learned or unlearned.
+     * @throws IOException in case file doesn't exist
+     */
+    @FXML
+    public void learned() throws IOException {
+        IndexCard card = set.getCard();
+        card.setLearned(checkBox.isSelected());
+        String CardFront = card.getFront();
+        String CardBack = card.getBack();
+        removeLineFromFile(CardFront);
+        File currentCourseSet = new File("userData/" + currentUser + "/" + currentUser+currentCourse+".txt");
+        BufferedWriter courseSetWriter = new BufferedWriter(new FileWriter(currentCourseSet,true));
+        currentCheck = card.isLearned();
+        courseSetWriter.write(CardFront+","+CardBack+","+currentCheck+",");
+        courseSetWriter.write("\n");
+        courseSetWriter.close();
+
+    }
+
+    @FXML
+    public void nextCard(){
+        set.nextCard();
+    }
+
+    @FXML
+    public void previousCard(){
+        set.previousCard();
+    }
+
+
+    /**
+     * Initializes any existing index cards and also creates the new Set for the index cards to sit in.
+     * @param location
+     * The location used to resolve relative paths for the root object, or
+     * <tt>null</tt> if the location is not known.
+     *
+     * @param resources
+     * The resources used to localize the root object, or <tt>null</tt> if
+     * the root object was not localized.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        File currentUserName = new File("currentUser.txt");
+        File currentCourseName = new File("currentCourseSelected.txt");
+
+
+        //Scanner creator to read currentUser (Helps with accessing the file)
+        Scanner reader;
+        try {
+            reader = new Scanner(currentUserName);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Scanner creator for reading the current Course name.
+        Scanner readCourseName;
+        try{
+            readCourseName = new Scanner(currentCourseName);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // currentUser reads current username,  currentCourse reads the current course that is open.
+        currentUser = reader.nextLine();
+        currentCourse = readCourseName.nextLine();
+        reader.close();
+        readCourseName.close();
+
+        File theExistentCourse = new File("userData/"+currentUser+"/"+currentUser+currentCourse+".txt");
+
+
+
+        set = new Set(currentCourse, text, checkBox, counterBox);
+        if(theExistentCourse.exists())
+        {
+            Scanner ExistentCourse;
+            try {
+
+                ExistentCourse = new Scanner(theExistentCourse);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            if(theExistentCourse.length() != 0)
+            {
+                while (ExistentCourse.hasNextLine()) {
+                    String[] details = ExistentCourse.nextLine().split(",");
+                    boolean checked = Boolean.parseBoolean(details[2]);
+                    set.addCard(details[0], details[1], checked);
+                }
+            }
+            ExistentCourse.close();
+        }
+
+
+    }
+
+    /**
+     * Removes the String specified in the file inFile.
+     * @param courseToRemove String name of file to remove
+     */
+    public void removeLineFromFile(String courseToRemove) {
+
+        try {
+
+            File userReader = new File("currentUser.txt");
+            Scanner readUser = new Scanner(userReader);
+            currentUser = readUser.nextLine();
+            readUser.close();
+
+
+            File inFile = new File("userData/"+currentUser+"/"+currentUser+currentCourse+".txt");
+            BufferedReader br = new BufferedReader(new FileReader(inFile));
+
+            if (!inFile.isFile()) {
+                System.out.println("Parameter is not an existing file");
+                return;
+            }
+
+            //Construct the new file that will later be renamed to the original filename.
+            File tempFile = new File(inFile.getAbsolutePath() + ".tmp");
+
+            //BufferedReader br = new BufferedReader(new FileReader());
+            PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+
+            String line;
+            //Read from the original file and write to the new
+            //unless content matches data to be removed.
+            while ((line = br.readLine()) != null) {
+
+                if (!line.trim().contains(courseToRemove)) {
+
+                    pw.println(line);
+                    pw.flush();
+                }
+
+
+            }
+            pw.close();
+            br.close();
+
+            //Delete the original file
+            inFile.delete();
+            //Rename the new file to the original file
+            tempFile.renameTo(inFile);
+
+
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Reviews all cards that are checkmarked learned
+     * @throws IOException In case file doesn't exist
+     */
+    @FXML
+    public void reviewLearned() throws IOException {
+        File currentCourseFile = new File("userData/"+currentUser+"/"+currentUser+currentCourse+".txt");
+        Scanner courseReader = new Scanner(currentCourseFile);
+        set = new Set(currentCourse, text, checkBox, counterBox);
+        while(courseReader.hasNextLine())
+        {
+            String[] readTheLine = courseReader.nextLine().split(",");
+            boolean checker = Boolean.parseBoolean(readTheLine[2]);
+            if(readTheLine[2].equals("true"))
+            {
+                set.addCard(readTheLine[0],readTheLine[1], checker);
+            }
+
+        }
+        courseReader.close();
+        //deleteCardB.setVisible(false);
+        checkBox.setVisible(false);
+        createCard.setVisible(false);
+
+        Stage stage = (Stage) reviewLearnedB.getScene().getWindow();
+        stage.setTitle("Review Learned");
+        stage.show();
+    }
+
+    /**
+     * Reviews all cards that are unchecked on the learned checkbox
+     * @throws IOException In case file doesn't exist
+     */
+    @FXML
+    public void reviewUnlearned() throws IOException {
+        File currentCourseFile = new File("userData/"+currentUser+"/"+currentUser+currentCourse+".txt");
+        Scanner courseReader = new Scanner(currentCourseFile);
+        set = new Set(currentCourse, text, checkBox, counterBox);
+        while(courseReader.hasNextLine())
+        {
+            String[] readTheLine = courseReader.nextLine().split(",");
+            Boolean checker = Boolean.parseBoolean(readTheLine[2]);
+            if(readTheLine[2].equals("false"))
+            {
+                set.addCard(readTheLine[0],readTheLine[1], checker);
+            }
+
+        }
+        courseReader.close();
+        //deleteCardB.setVisible(false);
+        checkBox.setVisible(false);
+        createCard.setVisible(false);
+        Stage stage = (Stage) reviewLearnedB.getScene().getWindow();
+        stage.setTitle("Review Not Learned");
+        stage.show();
+    }
+
+    /**
+     * Reviews all cards regardless of any parameters.
+     * @throws IOException in case file doesn't exist
+     */
+    @FXML
+    public void reviewAll() throws IOException
+    {
+        File currentCourseFile = new File("userData/"+currentUser+"/"+currentUser+currentCourse+".txt");
+        Scanner courseReader = new Scanner(currentCourseFile);
+        set = new Set(currentCourse, text, checkBox, counterBox);
+        while(courseReader.hasNextLine())
+        {
+            String[] readTheLine = courseReader.nextLine().split(",");
+            Boolean checker = Boolean.parseBoolean(readTheLine[2]);
+            set.addCard(readTheLine[0],readTheLine[1], checker);
+        }
+        courseReader.close();
+        //deleteCardB.setVisible(false);
+        checkBox.setVisible(false);
+        createCard.setVisible(false);
+        Stage stage = (Stage) reviewLearnedB.getScene().getWindow();
+        stage.setTitle("Review All");
+        stage.show();
+    }
+}
+
